13499ede1a50508df47a518d9ea8e38719ddc0b2
SandunWebDev <sandunwebdev@gmail.com>
2018-08-20 22:28:27 +0530

    Added ESLint & Prettier code formatting.

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 /**
- * Main file of Node + Express back-end server. 
- * This get executed when we run "npm start" and start a web server at port 3000. 
+ * Main file of Node + Express back-end server.
+ * This get executed when we run "npm start" and start a web server at port 3000.
  */
 
 const express = require("express");
 
-// Creating an instance of express app.  
+// Creating an instance of express app.
 const app = express();
 
 /* Routes - Defining what to do for each routes requests*/
 
-app.get("/", (req, res)=>{
-  res.json({status: "success"});
+app.get("/", (req, res) => {
+  res.json({ status: "success" });
 });
 
 // Start the server and listen at port 3000.
-app.listen(3000, ()=>console.log("Server Started At Port 3000"));
-
+app.listen(3000, () => console.log("Server Started At Port 3000"));
