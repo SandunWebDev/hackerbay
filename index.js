eb439aa9d202b812ef7ed4e61ed8deac202bba58
SandunWebDev <sandunwebdev@gmail.com>
2018-09-10 08:40:59 +0530

    Checking codecov reports

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("./database/connect");
 
 app.listen(config.server.PORT, () =>
   console.log(
-    `Server is running on "${config.server.NODE_ENV}" enviroment @ port ${
+    `Server is running on "${config.server.NODE_ENV}" enviroment at port ${
       config.server.PORT
     }.`
   )
