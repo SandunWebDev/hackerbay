67c35767fe85ed0a9b062469f23d15d0365960c7
SandunWebDev <sandunwebdev@gmail.com>
2018-12-13 17:00:40 +0530

    Update CI Pipe scripts.

--- docker/helpers/dockerHubUpload.sh
+++ docker/helpers/dockerHubUpload.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-# Script used in Travis CI to Build Production Image and Upload it to docker hub with appopriate tags.
+# This script used in Travis CI to Build Production Image using Docker and Upload it to Docker Bub with appopriate tags.
+
+# Bulding, Tagging and Pushing only happen for direct merges in master and release branches. 
+# "$TRAVIS_PULL_REQUEST = false" explicitly make sure anything don't get executed (even if this script executed) for intermediate pull request for those branches. 
+# (We can also just disable all pull request build from Travis CI settings.)
 
 # Building Production Build Image & Loggin In.
-if [ "$TRAVIS_BRANCH" == "master" ] ||  [ "$TRAVIS_BRANCH" == "release" ]
+if ([ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]) ||  ([ "$TRAVIS_BRANCH" == "release" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ])
 then
-   echo "Production Docker Image Built Is Started.";
+   echo "Production Docker Image Built Is Started for Branch $TRAVIS_BRANCH";
    docker-compose -f docker-compose.yml -f dc-production.yml build;
    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
 else
    echo "Production Docker Image Built Is Skipped. (Because not a master or release branch build)";
 fi
 
-# Tagging with appopriate tags and push them into docker hub.
-if [ "$TRAVIS_BRANCH" == "master" ]
+
+# Tagging with appopriate tags and push them into docker hub. 
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
   docker tag hb-backend-image-prod sandunwebdev/hb-backend-image-prod:master;
 
   docker push sandunwebdev/hb-backend-image-prod:master;
-elif [ "$TRAVIS_BRANCH" == "release" ]
+elif [ "$TRAVIS_BRANCH" == "release" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
   docker tag hb-backend-image-prod sandunwebdev/hb-backend-image-prod:release;
   docker tag hb-backend-image-prod sandunwebdev/hb-backend-image-prod:latest;
